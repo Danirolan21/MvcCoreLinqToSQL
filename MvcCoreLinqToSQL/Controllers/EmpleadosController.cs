b2850edb537073c8ed8178bee88e4eb0b9ac5e63
Danirolan21 <danirolan21@hotmail.com>
2025-02-05 11:36:57 +0100

    Add project files.

---
+++ MvcCoreLinqToSQL/Controllers/EmpleadosController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreLinqToSQL.Models;
+using MvcCoreLinqToSQL.Repositories;
+
+namespace MvcCoreLinqToSQL.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        RepositoryEmpleados repo;
+        public EmpleadosController()
+        {
+            this.repo = new RepositoryEmpleados();
+        }
+        public IActionResult Index()
+        {
+            List<Empleado> empleados = this.repo.GetEmpleados();
+            return View(empleados);
+        }
+
+        public IActionResult Details(int idEmpleado)
+        {
+            Empleado empleado = this.repo.FindEmpleado(idEmpleado);
+            return View(empleado);
+        }
+        public IActionResult BuscadorEmpleados()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BuscadorEmpleados(string oficio, int salario)
+        {
+            List<Empleado> empleados = 
+                this.repo.GetEmpleadosOficioSalario(oficio, salario);
+            if (empleados == null)
+            {
+                ViewData["MENSAJE"] = "No existen empleados con oficio " +
+                    oficio + " y salario superior a " + salario;
+                return View();
+            } else
+            {
+                return View(empleados);
+            }
+        }
+    }
+}
